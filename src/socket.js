c2e072410ba90bb1e1fe424db41e7eec2ed47002
Tuyết Anh <tuyetanh82198@gmail.com>
2023-10-31 10:58:53 +0700

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 export const socket = io("https://post-app-backend-dr19.onrender.com", {
   autoConnect: false,
   extraHeaders: {
-    "Access-Control-Allow-Origin": "https://post-app-demo.web.app",
+    "Access-Control-Allow-Origin": "https://post-app-2rt8.onrender.com",
     "Access-Control-Allow-Credentials": "true",
   },
   withCredentials: true,
